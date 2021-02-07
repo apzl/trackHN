78de8285a7ad712ad75b8e1a0d5b784348ce7c5b
apzl <apsalshbk550@gmail.com>
2021-02-07 21:27:54 +0530

    final

--- README.md
+++ README.md
@@ -4,10 +4,9 @@
 
 [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://trackhn.herokuapp.com)  
 
-This is simple application to search posts in hackernews for keywords. A usual search for a keyword, say 'go' as in go lang would give results containg 'Go Daddy', the board game Go and even the verb go. This ambiguity in meaning is very common among many words like bank, plane, mouse and many more.  
-This problem can be solved to an extent using the contextual word embedding from the moder transformer based language models. Same words in different context are represented by different vectors in the language models like ELMo, BERT, GPT etc.  
+This is simple application to search posts in hackernews for keywords. A usual search for a keyword, say 'go' as in Go lang would give results containing 'Go Daddy', the board game Go, and even the verb go. This ambiguity in meaning is very common among many words like bank, plane, mouse and many more.  
+This problem can be solved to an extent using the contextual word embedding from the modern transformer based language models. Same words in different context are represented by different vectors in the language models like ELMo, BERT, GPT etc.  
 
-![](http://ai.stanford.edu/blog/assets/img/posts/2020-03-24-contextual/contextual_mouse_transparent_1.png)
-![](http://ai.stanford.edu/blog/assets/img/posts/2020-03-24-contextual/contextual_mouse_transparent_2.png)
+![](http://ai.stanford.edu/blog/assets/img/posts/2020-03-24-contextual/contextual_mouse_transparent_1.png)![](http://ai.stanford.edu/blog/assets/img/posts/2020-03-24-contextual/contextual_mouse_transparent_2.png)
 
 
--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import streamlit as st
+
 import time
 import re
 import requests 
-from utils import word_embed, highlight
-from transformers import BertTokenizer, BertModel
 from scipy.spatial.distance import cosine
+
+from utils import word_embed, highlight
+
+
+from transformers import BertTokenizer, BertModel
+
 
 
 threshold = st.sidebar.slider(
@@ -12,6 +17,9 @@ threshold = st.sidebar.slider(
     0.1, 0.99, (0.6)
 )
 category = st.sidebar.radio('category',['new','top','best'])    
+
+
+
 
 @st.cache
 def load_model():
@@ -22,21 +30,31 @@ def load_model():
 	return model,tokenizer
 
 
+
+
 def main():
   st.markdown("<h1 style='text-align: center; color: rgb(246, 51, 102);'>trackHN</h1>", unsafe_allow_html=True)	
   st.markdown("<p style='text-align: center;'>track HackerNews, with sense</p>",unsafe_allow_html=True)
   keyword = st.text_input("")
   context = st.text_area("sample text")
   search = st.button("go")
+
+
+
   if search:
-    if(keyword.lower() in context.lower()):
+    #check if keyword in sample text
+    if(re.search(r'\b'+keyword+r'\b',context,flags=re.IGNORECASE)):
       model,tokenizer = load_model()
+      #gets embedding for the keyword
       context_embed = word_embed(context, keyword, model, tokenizer)
+      
+      
       with st.spinner('searching...'):
         base_url = 'https://hacker-news.firebaseio.com/v0/' 
         end_url = '.json?print=pretty'
         ids_url = ''.join([base_url,category,'stories',end_url])
-        ids = requests.get(ids_url)
+        #gets ids of 500 posts             
+        ids = requests.get(ids_url) 
         for id in ids.json():
           url = ''.join([base_url,'item/',str(id),end_url])
           r = requests.get(url).json()
@@ -45,19 +63,26 @@ def main():
             title = r['title']
             match = (re.search(r'\b'+keyword+r'\b',title,flags=re.IGNORECASE))
             if match:
-              index = match.start()
-              title_embed = word_embed(title,keyword,model, tokenizer)
+              index = match.start() 
+              #gets embedding for keyword in search result                                          
+              title_embed = word_embed(title,keyword,model, tokenizer)        
               similarity=(1-cosine(context_embed,title_embed))
               if (similarity>threshold):
                 before = title[:index]
                 key = title[index:index+len(keyword)]
                 after = title[index+len(keyword):]
-                r,g,b=highlight(similarity)              #calculate the intensity of keyword highlighting
+                #calculate the intensity of keyword highlighting
+                h,s,l=highlight(similarity)                                   
+                
+                
+                #formatting output text                                
                 st.markdown("<style>.before{background-color:0;margin:0; display:inline;}</style>",unsafe_allow_html=True)
                 st.markdown("<style>.after{background-color:0;margin:0; display:inline;}</style>",unsafe_allow_html=True)
                 first = "<div><p class=before>{}</p><a style = 'background-color:".format(before)
-                st.markdown(first+"hsl({},{}%,{}%);'>{}</a><p class=after>{}</p></div>".format(r,g,b,key,after), unsafe_allow_html=True)
+                st.markdown(first+"hsl({},{}%,{}%);'>{}</a><p class=after>{}</p></div>".format(h,s,l,key,after), unsafe_allow_html=True)
                 st.markdown("<a href={}>read more</a>".format(link),unsafe_allow_html=True)
+    
+    
     else:
       st.write('keyword missing in text')
 
